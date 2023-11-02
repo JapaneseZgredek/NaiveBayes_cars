0482a74a34e5eb9572961121c301d702823b9a0f
JapaneseZgredek <84537728+JapaneseZgredek@users.noreply.github.com>
2023-11-02 16:20:26 +0100

    Sepereate data into training and testing

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from Specifications import Specifications
 
 
-def read_from_file() -> list:
-    data = []
+def read_from_file():
+    training_data = []
+    testing_data = []
     with open("car_evaluation.data", "r") as file:
-        for line in file:
+        lines = file.readlines()
+        for i, line in enumerate(lines):
             line = line.split(',')
-            data.append(Specifications(line[:6], line[len(line) - 1].removesuffix('\n')))
-    return data
+            if i < len(lines)*0.7:
+                testing_data.append(Specifications(line[:6], line[len(line) - 1].removesuffix('\n')))
+            else:
+                training_data.append(Specifications(line[:6], line[len(line) - 1].removesuffix('\n')))
+    return training_data, testing_data
 
 
 def main():
-    data = read_from_file()
-    for element in data:
+    training_data, testing_data = read_from_file()
+    for element in training_data:
         element.to_string()
-
+    for element in testing_data:
+        element.to_string()
+    print(len(testing_data))
+    print(len(training_data))
 
 
 if __name__ == '__main__':
