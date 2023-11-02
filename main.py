cb109f53010077385ae58e7cd70083d3bb14a8cf
JapaneseZgredek <84537728+JapaneseZgredek@users.noreply.github.com>
2023-11-02 16:49:39 +0100

    cut data, according to opinion. Count different values of every attribute

--- main.py
+++ main.py
@@ -15,14 +15,38 @@ def read_from_file():
     return training_data, testing_data
 
 
+def cut_data_into_lists(data: list):
+    vgood, acc, good, unacc = [], [], [], []
+    for specification in data:
+        if specification.opinion == 'vgood':
+            vgood.append(specification)
+        elif specification.opinion == 'acc':
+            acc.append(specification)
+        elif specification.opinion == 'good':
+            good.append(specification)
+        else:
+            unacc.append(specification)
+
+    return vgood, acc, good, unacc
+
+
+def create_dictionaries_for_specific_opinion(opinion_list: list) -> list:
+    dictionaries_list = [{}, {}, {}, {}, {},{}]
+    for specification in opinion_list:
+        for i in range(len(specification.attributes)):
+            if specification.attributes[i] in dictionaries_list[i]:
+                dictionaries_list[i][specification.attributes[i]] += 1
+            else:
+                dictionaries_list[i][specification.attributes[i]] = 1
+
+    return dictionaries_list
+
+
 def main():
     training_data, testing_data = read_from_file()
-    for element in training_data:
-        element.to_string()
-    for element in testing_data:
-        element.to_string()
-    print(len(testing_data))
-    print(len(training_data))
+    vgood, acc, good, unacc = cut_data_into_lists(training_data)
+    data_dictionary = create_dictionaries_for_specific_opinion(training_data)
+    print(data_dictionary)
 
 
 if __name__ == '__main__':
